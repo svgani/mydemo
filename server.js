a7ee76a71957ee6bc1b78de773cfef6b75dd4392
svgani <prabha.chekkapalli@gmail.com>
2021-01-05 20:06:17 +0530

    removed some dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-var path = require('path');        
-const port = process.env.PORT ||3000;   
+var path = require('path');
+const port = process.env.PORT ||3000;
 
 
 //Set the base path to the angular-test dist folder
