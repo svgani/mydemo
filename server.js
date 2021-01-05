6e9392315457d1663251ea51aa5ade80e45e0d16
Srinivasu Gannem <srinivasu.gannem@nxp.com>
2021-01-05 06:31:22 +0530

    added server.js and expres and path dependencies

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+var path = require('path');        
+const port = process.env.PORT ||3000;   
+
+
+//Set the base path to the angular-test dist folder
+app.use(express.static(path.join(__dirname, 'dist/mydemo')));
+
+//Any routes will be redirected to the angular app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/mydemo/index.html'));
+});
+
+//Starting server on port 8081
+app.listen(port, () => {
+    console.log('Server started!');
+    console.log(port);
+});
